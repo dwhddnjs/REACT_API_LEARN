3ca827856af9fac6ce6126299b56e1be24cbb82a
dwhddnjs <per1215@naver.com>
2021-07-02 22:47:49 +0900

    removed useless code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Users from './Users';
+import "./App.css";
+import Users from "./Users";
 
 function App() {
-  return (
-    <Users />
-  );
+  return <Users />;
 }
 
 export default App;
