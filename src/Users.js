303149474a01e1a9e2db3d1e84de1c6a40ac5d77
dwhddnjs <per1215@naver.com>
2021-07-02 22:20:05 +0900

    useReducer

--- src/Users.js
+++ src/Users.js
@@ -1,43 +1,72 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, useReducer } from "react";
+import axios from "axios";
 
-function Users() {
-
-    const [users, setUsers] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-
-    const fetchUsers = async () => {
-        try {
-            setUsers(null)
-            setError(null)
-            setLoading(true)
-            const response = await axios.get()
-            setUsers(response.data)
-        } catch (e) {
-            setError(e)
-            console.log(e.response.status)
-        }
-        setLoading(false)
-    }
-
-    useEffect(() => {
-        fetchUsers()
-    }, [])
-
-    if(loading) return <div>로딩중..</div>
-    if(error) return <div>에러가 발생함</div>
-    if(!users) return null
-
-
-    return (
-        <>
-        <ul>
-            {users.map(user => <li key={user.id}>{user.username} ({user.name})</li>)}
-        </ul>
-        <button onClick={fetchUsers}>다시 불러오기</button>
-        </>
-    )
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
 }
 
-export default Users
+function Users() {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  const fetchUsers = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const response = await axios.get(
+        "https:jsonplaceholder.typicode.com/users"
+      );
+      dispatch({ type: "SUCCESS", data: response.data });
+    } catch (e) {
+      console.log(e.response.status);
+      dispatch({ type: "ERROR", error: e });
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const { loading, data: users, error } = state;
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생함</div>;
+  if (!users) return null;
+
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
+  );
+}
+
+export default Users;
